4f49a3cdd003e339d70ae0a26fd8b4b3c9582acf
Muskan <muskankhushi2022@gmail.com>
2024-08-06 16:09:16 +0530

    .

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -1,10 +1,23 @@
 'use client'
 
+import { AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import { Accordion } from '@radix-ui/react-accordion'
 import { FC } from 'react'
+import ChatHeader from './ChatHeader'
 
 
 const Chat: FC = ({}) => {
-  return <div>Chat</div>
+  return <Accordion type='single' collapsible className='relative bg-white z-40 shadow'>
+    <AccordionItem value='item-1'>
+      <div className='fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden'>
+        <div className='w-full h-full flex flex-col'>
+          <AccordionTrigger className='px-6 border-b border-zinc-300'>
+            <ChatHeader />
+          </AccordionTrigger>
+        </div>
+      </div>
+    </AccordionItem>
+  </Accordion>
 }
 
 export default Chat
